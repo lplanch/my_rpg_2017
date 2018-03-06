b7a87ae39e8ff032677607d11a6aeb88ba1d8942
Mauconduit Thomas <Mauc@localhost.localdomain>
2018-03-06 13:45:22 +0100

    add test

---
+++ mauc_test/include/my_rpg.h
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2017
+** my_rpg.h
+** File description:
+** .h file to stock all functions from our screensaver librairy
+*/
+
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <errno.h>
+#include <stdio.h>
+#include <SFML/Audio.h>
+#include <SFML/Graphics.h>
+
+#include "struct.h"
+
+#ifndef MY_COOK_
+#define MY_COOK_
+
+#endif
