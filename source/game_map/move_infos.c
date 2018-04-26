a59d7388f1ab5cc35cf09e33f5286f49fad86b15
Mauconduit Thomas <Mauc@localhost.localdomain>
2018-04-26 21:27:07 +0200

    remove pb quests

--- source/game_map/move_infos.c
+++ source/game_map/move_infos.c
@@ -22,11 +22,11 @@ sfVector2f pos_quests_des_text)
 void move_infos_box(st_rpg *s)
 {
 	sfVector2f pos_quests_box =
-	create_vector2f(s->fi->camera.x - 960, s->fi->camera.y - 510);
+	create_vector2f(s->fi->camera.x - 960, s->fi->camera.y - 540);
 	sfVector2f pos_quests_box_text =
 	create_vector2f(pos_quests_box.x + 10, pos_quests_box.y + 27);
 	sfVector2f pos_quests_des =
-	create_vector2f(s->fi->camera.x - 960, s->fi->camera.y - 510 +
+	create_vector2f(s->fi->camera.x - 960, s->fi->camera.y - 540 +
 	s->fi->quests_box.quests_box->obj->rect.height);
 	sfVector2f pos_quests_des_text =
 	create_vector2f(pos_quests_box.x + 10, pos_quests_box.y + 90);
