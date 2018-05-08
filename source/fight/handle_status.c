ce7cbfbc7eb5773d563419c6b68cdd82e676525c
Martin Galoux <mgaloux@localhost.localdomain>
2018-05-08 15:20:55 +0200

    poison

--- source/fight/handle_status.c
+++ source/fight/handle_status.c
@@ -8,6 +8,15 @@
 #include "my_rpg.h"
 #include "my.h"
 
+void handle_poison(st_rpg *s, enemy_t *mob)
+{
+	if (s->player.cdata.classe == 2 && s->player.tree.passive == 1 &&
+		!s->player.tree.lock[1]) {
+		mob->poison->amount = 1;
+		mob->poison->count = 1;
+	}
+}
+
 void handle_lifesteal(st_rpg *s, float amount, effect_t *effect)
 {
 	if (effect->count > 0)
