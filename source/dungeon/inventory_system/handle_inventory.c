5a4751593afd939e439c03a83101b3c356f648c5
planch2 <planch2@localhost.localdomain>
2018-05-08 12:07:14 +0200

    inventory main functions

--- source/dungeon/inventory_system/handle_inventory.c
+++ source/dungeon/inventory_system/handle_inventory.c
@@ -11,7 +11,7 @@
 void clear_inventory_slot(item_t *slot)
 {
 	slot->id = 0;
-	slot->stacks = 0;
+	slot->stacks = 1;
 	slot->sprite = NULL;
 }
 
@@ -27,9 +27,36 @@ void add_inventory_slot(item_t *first_slot)
 	conductor->next->next = NULL;
 }
 
-void add_inventory_item(item_t *first_slot, unsigned int id)
+int search_item(item_t *first_slot, unsigned int id)
 {
-	
+	item_t *current;
+
+	while (current != NULL) {
+		if (current->id == id)
+			return (current->pos);
+		current = current->next;
+	}
+	return (-1);
+
+}
+
+int add_inventory_item(item_t *first_slot, unsigned int id)
+{
+	item_t *current;
+	int pos = search_item(first_slot, id);
+
+	if (pos == -1) {
+		while (current != NULL && current->sprite != NULL)
+			current = current->next;
+		if (current == NULL)
+			return (0);
+		current->stacks += 1;
+	} else {
+		while (current->pos < pos)
+			current = current->next;
+		current->stacks += 1;
+	}
+	return (1);
 }
 
 void remove_inventory_slot(item_t *first_slot)
