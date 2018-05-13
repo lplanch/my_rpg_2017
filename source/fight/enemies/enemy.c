c9b3b0164f55af23fc33588c5093a10e167bbd9f
Martin Galoux <mgaloux@localhost.localdomain>
2018-05-13 13:40:15 +0200

    6 NEW ENEMIES WOW

--- source/fight/enemies/enemy.c
+++ source/fight/enemies/enemy.c
@@ -25,16 +25,37 @@ void update_enemies(st_rpg *s)
 	enemies_animation(s);
 }
 
+char *get_enemy_path(void)
+{
+	switch (rand() % 6) {
+		case 0 :
+			return ("ressources/enemies/Fanatic");
+		case 1 :
+			return ("ressources/enemies/Blob");
+		case 2 :
+			return ("ressources/enemies/Bat");
+		case 3 :
+			return ("ressources/enemies/Demon");
+		case 4 :
+			return ("ressources/enemies/Minotaur");
+		case 5 :
+			return ("ressources/enemies/Ghost");
+	}
+	return (NULL);
+}
+
 void generate_enemies(st_rpg *s)
 {
 	s->proc.pvar.enemy_nbr = s->proc.pvar.min_enemies + rand() %
 	(s->proc.pvar.max_enemies - s->proc.pvar.min_enemies);
 	s->f.mob = malloc(sizeof(enemy_t *) * s->proc.pvar.enemy_nbr);
 	for (int i = 0; i < s->proc.pvar.enemy_nbr; i++) {
-		s->f.mob[i] = generate_enemy("ressources/enemies/Fanatic");
+		s->f.mob[i] = generate_enemy(get_enemy_path());
 		s->f.mob[i]->obj->pos = create_ennemy_position(s);
 		sfSprite_setPosition(s->f.mob[i]->obj->sprite,
 		s->f.mob[i]->obj->pos);
+		sfSprite_setScale(s->f.mob[i]->obj->sprite,
+		(sfVector2f){s->f.mob[i]->scale.x, s->f.mob[i]->scale.y});
 	}
 	generate_boss(s);
 }
